7eec9505ca45872853db68ff4fe567a32f9d39e7
Caleb Wardlaw <calebwardlaw@gmail.com>
2022-11-01 18:04:05 -0400

    Main view btns

--- app/src/main/java/com/example/c196/MainActivity.java
+++ app/src/main/java/com/example/c196/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.c196;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toTerms(View view) {
+    }
+
+    public void toCourses(View view) {
+    }
+
+    public void toAssesments(View view) {
     }
 }
